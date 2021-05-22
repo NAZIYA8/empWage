3911a948aa330ab65a15813a57f3a7135ba06884
NAZIYA8 <syeda.naziya896@gmail.com>
2021-05-23 00:52:10 +0530

    Added: emp attendance

--- empwageComputation.sh
+++ empwageComputation.sh
@@ -1,1 +1,11 @@
-#welcome to emp wage computation
+#!/bin/bash -x
+
+empCheck=$((RANDOM%2))
+isPresent=1
+
+if [ $empcheck -eq $isPresent ]
+then
+	echo Employee is present
+else
+	echo Employee is absent
+fi
